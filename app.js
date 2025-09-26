1151b836c84161b33065fd1dd822fb5332231da4
Aish Waheed <64914670+aish8la@users.noreply.github.com>
2025-09-27 01:41:45 +0500

    feat: setup EJS template engine and views folder

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 require('dotenv').config();
+const path = require('node:path');
+
 const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT;
+
+/* Set the template engine to EJS */
+
+app.set('view engine', 'ejs');
+
+/* Set the root views path for views */
+app.set('views', path.join(__dirname, 'views'));
 
 app.listen(PORT, (err) => {
     if(err) throw err;
