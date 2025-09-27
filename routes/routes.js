f55a59ca7039bbb68bd2e8da5cb3fe4d32c21d70
Aish Waheed <64914670+aish8la@users.noreply.github.com>
2025-09-27 17:20:25 +0500

    feat: add id for each message and middleware to get message by id

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,12 @@
 const messages = [
   {
+    msgId: crypto.randomUUID(),
     text: "Hi there!",
     user: "Amando",
     added: new Date()
   },
   {
+    msgId: crypto.randomUUID(),
     text: "Hello World!",
     user: "Charles",
     added: new Date()
@@ -21,12 +23,19 @@ const goToNewMessageForm = (req, res) => {
 
 const createNewMessage = (req, res) => {
   const message = req.body;
-  messages.push({ text: message.message, user: message.name, added: new Date() });
+  messages.push({ msgId: crypto.randomUUID(), text: message.message, user: message.name, added: new Date() });
   res.redirect('/');
+}
+
+const getMessageById = (req, res) => {
+  const id = req.params.msgId;
+  const message = messages.find(message => message.msgId === id);
+  res.render('message', { title: 'Message', page: 'message', message: message});
 }
 
 module.exports = {
   serveIndexPage: serveIndexPage,
   goToNewMessageForm: goToNewMessageForm,
-  createNewMessage: createNewMessage
+  createNewMessage: createNewMessage,
+  getMessageById: getMessageById
 };
