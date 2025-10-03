477f9e68e55e0cff3715eb69013393e18a51ffca
Aish Waheed <64914670+aish8la@users.noreply.github.com>
2025-10-03 06:57:07 +0500

    build: add connection string db option

--- db/pool.js
+++ db/pool.js
@@ -1,12 +1,20 @@
 const { Pool } = require('pg');
 
-const dbConfig = { 
-    port: process.env.PORT,
-    host: process.env.PGHOST,
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    database: process.env.PGDATABASE,
-    port: process.env.PGPORT
+const connectionString = process.env.DATABASE_URL;
+
+let dbConfig;
+
+if (connectionString) {
+    dbConfig = { connectionString: connectionString}
+} else {
+    dbConfig = { 
+        port: process.env.PORT,
+        host: process.env.PGHOST,
+        user: process.env.PGUSER,
+        password: process.env.PGPASSWORD,
+        database: process.env.PGDATABASE,
+        port: process.env.PGPORT
+    }
 };
 
 const pool = new Pool(dbConfig);
